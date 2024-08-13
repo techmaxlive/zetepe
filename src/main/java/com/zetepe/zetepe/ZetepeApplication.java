aae30b36fc1df3c856fac0c53e7b8a6f99a94695
ranjithkumar <ranjithkumarsathaiyan@gmail.com>
2024-08-13 13:04:55 +0530

    first commit

---
+++ src/main/java/com/zetepe/zetepe/ZetepeApplication.java
@@ -0,0 +1,13 @@
+package com.zetepe.zetepe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZetepeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZetepeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zetepe/zetepe/ZetepeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zetepe.zetepe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZetepeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
